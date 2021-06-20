7a6ffd43d807f9148c9c15f0f78f52b36fea48be
Saikat Paul <saptadipa123@gmail.com>
2021-06-20 20:27:00 +0530

    Mod

--- myreduce.js
+++ myreduce.js
@@ -1,5 +1,5 @@
-Array.prototype.myReduce = function(callback) {
-  let value = 0;
+Array.prototype.myReduce = function(callback, initialValue) {
+  let value = initialValue;
   for (let i = 0; i < this.length; i++) {
     value = callback(value, this[i], i, this);
   }
